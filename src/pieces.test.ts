80d44d766bec53bc8890e9725ff287f4e793af0f
Felipe-53 <felipeandresb97@gmail.com>
2023-04-26 21:23:39 -0300

    update tests

--- src/pieces.test.ts
+++ src/pieces.test.ts
@@ -3,9 +3,10 @@ import { Bishop, Tower } from "./pieces";
 
 test("Bishop", () => {
   let bishop = new Bishop("black");
-  const paths = bishop.getPaths([0, 0]);
 
-  const moves = [];
+  let paths = bishop.getPaths([0, 0]);
+
+  let moves = [];
   for (const path of paths) {
     moves.push(...path);
   }
@@ -22,13 +23,16 @@ test("Bishop", () => {
       moves.some((move) => move.toString() === position.toString())
     ).toBeTruthy();
   }
+
+  expect(diagonal.length).toBe(moves.length);
 });
 
 test("tower", () => {
-  let tower = new Tower("white");
-  const paths = tower.getPaths([0, 0]);
+  const tower = new Tower("white");
 
-  const moves = [];
+  let paths = tower.getPaths([0, 0]);
+
+  let moves = [];
   for (const path of paths) {
     moves.push(...path);
   }
@@ -46,10 +50,37 @@ test("tower", () => {
   ).toBeTruthy();
 
   expect(
-    moves.some((move) => move.toString() === [0, 2].toString())
+    moves.some((move) => move.toString() === [1, 0].toString())
   ).toBeTruthy();
 
   expect(
     moves.some((move) => move.toString() === [1, 1].toString())
   ).toBeFalsy();
+
+  paths = tower.getPaths([4, 4]);
+
+  moves = [];
+  for (const path of paths) {
+    moves.push(...path);
+  }
+
+  expect(
+    moves.some((move) => move.toString() === [4, 4].toString())
+  ).toBeFalsy();
+
+  expect(
+    moves.some((move) => move.toString() === [4, 5].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [5, 4].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [7, 4].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [4, 7].toString())
+  ).toBeTruthy();
 });
