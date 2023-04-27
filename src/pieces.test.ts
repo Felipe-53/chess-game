cd8bbb07fdc05609c4a24c58e596cf7f1d032e70
Felipe-53 <felipeandresb97@gmail.com>
2023-04-26 21:11:27 -0300

    add pieces tests

---
+++ src/pieces.test.ts
@@ -0,0 +1,59 @@
+import { expect, test } from "vitest";
+import { Bishop, Tower } from "./pieces";
+
+test("Bishop", () => {
+  let bishop = new Bishop("black");
+  const paths = bishop.getPaths([0, 0]);
+
+  const moves = [];
+  for (const path of paths) {
+    moves.push(...path);
+  }
+
+  console.log(moves);
+
+  let i = 1;
+  const diagonal = [];
+  while (i < 8) {
+    diagonal.push([i, i]);
+    i++;
+  }
+
+  console.log(diagonal);
+
+  for (const position of diagonal) {
+    expect(
+      moves.some((move) => move.toString() === position.toString())
+    ).toBeTruthy();
+  }
+});
+
+test("tower", () => {
+  let tower = new Tower("white");
+  const paths = tower.getPaths([0, 0]);
+
+  const moves = [];
+  for (const path of paths) {
+    moves.push(...path);
+  }
+
+  expect(
+    moves.some((move) => move.toString() === [0, 7].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [7, 0].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [0, 1].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [0, 2].toString())
+  ).toBeTruthy();
+
+  expect(
+    moves.some((move) => move.toString() === [1, 1].toString())
+  ).toBeFalsy();
+});
