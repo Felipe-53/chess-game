60ad82984c529f021d72e0fe4691a4addbdf1dba
Felipe-53 <felipeandresb97@gmail.com>
2023-04-27 16:19:17 -0300

    pawn tests

--- src/chess.test.ts
+++ src/chess.test.ts
@@ -1,6 +1,6 @@
 import { test, expect, describe, beforeEach } from "vitest";
 import { Board, Chess } from "./chess";
-import { Bishop, Tower } from "./pieces";
+import { Bishop, Pawn, Queen, Tower } from "./pieces";
 import { Position } from "./types";
 
 let board: Board;
@@ -55,4 +55,79 @@ test("Cannot move a constrained tower", () => {
   const moves = chess.getValidMoves([0, 0]);
 
   expect(moves.length).toBe(0);
+});
+
+describe("Pawn special moves", () => {
+  test("Black pawn", () => {
+    let pawn = new Pawn("black");
+    let pawnPosition: Position = [1, 2];
+    let singleStep: Position = [2, 2];
+    let doubleStep: Position = [3, 2];
+    let diagonal1: Position = [2, 3];
+    let diagonal2: Position = [2, 1];
+
+    board.set(pawnPosition, pawn);
+    let moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).toContainEqual(singleStep);
+    expect(moves).toContainEqual(doubleStep);
+    expect(moves).not.toContainEqual(diagonal1);
+    expect(moves).not.toContainEqual(diagonal2);
+
+    board.set(diagonal1, new Bishop("white"));
+    board.set(diagonal2, new Bishop("white"));
+
+    moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).toContainEqual(diagonal1);
+    expect(moves).toContainEqual(diagonal2);
+  });
+
+  test("White pawn", () => {
+    let pawn = new Pawn("white");
+    let pawnPosition: Position = [6, 2];
+    let singleStep: Position = [5, 2];
+    let doubleStep: Position = [4, 2];
+    let diagonal1: Position = [5, 3];
+    let diagonal2: Position = [5, 1];
+
+    board.set(pawnPosition, pawn);
+    let moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).toContainEqual(singleStep);
+    expect(moves).toContainEqual(doubleStep);
+    expect(moves).not.toContainEqual(diagonal1);
+    expect(moves).not.toContainEqual(diagonal2);
+
+    board.set(diagonal1, new Queen("black"));
+    board.set(diagonal2, new Queen("black"));
+
+    moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).toContainEqual(diagonal1);
+    expect(moves).toContainEqual(diagonal2);
+  });
+
+  test("Cannot make double-step move unless in initial position", () => {
+    let pawn = new Pawn("white");
+    let pawnPosition: Position = [5, 1];
+    let doubleStep: Position = [3, 1];
+
+    const board = new Board();
+    board.set(pawnPosition, pawn);
+
+    let moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).not.toContainEqual(doubleStep);
+
+    pawn = new Pawn("black");
+    pawnPosition = [2, 1];
+    doubleStep = [4, 1];
+
+    board.set(pawnPosition, pawn);
+
+    moves = pawn.getValidMoves(pawnPosition, board);
+
+    expect(moves).not.toContainEqual(doubleStep);
+  });
 });
