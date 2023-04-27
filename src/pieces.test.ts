2e87fab9235ea0c0db635c23169e64bffbc1f6be
Felipe-53 <felipeandresb97@gmail.com>
2023-04-26 23:31:42 -0300

    tests king

--- src/pieces.test.ts
+++ src/pieces.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
-import { Bishop, Tower } from "./pieces";
+import { Bishop, King, Tower } from "./pieces";
+import { Position } from "./types";
 
 // TODO: some wisdom: testing the individual paths makes more sense
 // then, a simples integration test with the pieces would suffice
@@ -87,3 +88,64 @@ test("tower", () => {
     moves.some((move) => move.toString() === [4, 7].toString())
   ).toBeTruthy();
 });
+
+test("King", () => {
+  const king = new King("white");
+
+  let position = [4, 4] as Position;
+
+  let paths = king.getPaths(position);
+
+  let moves = [];
+  for (const path of paths) {
+    moves.push(...path);
+  }
+
+  let allDirections = getAllDirectionsOneUnity(position);
+
+  for (const position of allDirections) {
+    expect(
+      moves.some((move) => move.toString() === position.toString())
+    ).toBeTruthy();
+  }
+
+  position = [0, 0];
+
+  paths = king.getPaths(position);
+
+  moves = [];
+  for (const path of paths) {
+    moves.push(...path);
+  }
+
+  allDirections = getAllDirectionsOneUnity(position);
+
+  for (const position of allDirections) {
+    expect(
+      moves.some((move) => move.toString() === position.toString())
+    ).toBeTruthy();
+  }
+});
+
+function getAllDirectionsOneUnity(position: Position) {
+  let [iValue, jValue] = position;
+
+  const allDirections = [];
+  for (const i of [-1, 1, 0]) {
+    for (const j of [-1, 1, 0]) {
+      if (i === 0 && j === 0) continue;
+      let iResult = iValue + i;
+      let jResult = jValue + j;
+      if (
+        iResult >= 0 &&
+        iResult <= 7 &&
+        jResult + j >= 0 &&
+        jResult + j <= 7
+      ) {
+        allDirections.push([iResult, jResult]);
+      }
+    }
+  }
+
+  return allDirections;
+}
