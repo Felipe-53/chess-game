ae9d6234f3442c359e2e16963e53278963c5cf8a
Felipe-53 <felipeandresb97@gmail.com>
2023-05-02 19:44:29 -0300

    build only available on desktop

--- frontend/src/components/ChooseScreen.tsx
+++ frontend/src/components/ChooseScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export type Screen = "build" | "play-new" | "play-built";
 
@@ -7,17 +7,27 @@ interface Props {
 }
 
 const ChooseScreen: React.FC<Props> = ({ setScreen }) => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+
   return (
     <div className="bg-black my-auto absolute inset-0 flex flex-col items-center justify-center p-10 gap-10 mx-auto w-full">
       <h1 className="text-5xl font-bold text-gray-100">Build or Play</h1>
 
       <div className="flex justify-around w-72">
-        <button
-          className="btn btn-secondary"
-          onClick={() => setScreen("build")}
-        >
-          Build
-        </button>
+        {isMobile ? null : (
+          <button
+            className="btn btn-secondary"
+            onClick={() => setScreen("build")}
+          >
+            Build
+          </button>
+        )}
         <button
           className="btn btn-primary"
           onClick={() => setScreen("play-new")}
@@ -25,6 +35,10 @@ const ChooseScreen: React.FC<Props> = ({ setScreen }) => {
           Play
         </button>
       </div>
+
+      <p className="pt-4 text-secondary">
+        <b>*</b> Build only available on desktop
+      </p>
     </div>
   );
 };
